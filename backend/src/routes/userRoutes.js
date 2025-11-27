5da51043e0f0bd3d6393019d6ce0705bb0db202f
Pedro-Artur-RF <pedro.artur.rfn@gmail.com>
2025-11-27 10:48:08 -0300

    Changes to be committed: 	modified:   backend/src/routes/userRoutes.js 	modified:   package-lock.json

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -8,6 +8,6 @@ router.post("/", userController.createUsuario);
 router.put("/:id", userController.updateUsuario);
 router.delete("/:id", userController.deleteUsuario);
 router.post("/login", userController.loginUsuario);
-
+router.get("/:id", userController.getUsuarioById);
 
 export default router;
